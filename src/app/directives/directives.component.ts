b22c6dacda54afab34ab8106f6f9d6f6242e288a
Michele Paternoster <michele.paternoster@finconsgroup.com>
2020-02-25 17:51:24 +0100

    continua directives

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directives.component.css']
 })
 export class DirectivesComponent implements OnInit {
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5]
+  evenNumbers = [2, 4]
+  onlyOdd = false;
 
   constructor() { }
 
