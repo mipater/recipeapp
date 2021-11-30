337da413303966e23cb5ff42adf3e857beaa712d
Michele Paternoster <michele.paternoster@finconsgroup.com>
2021-11-30 11:54:34 +0100

    ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {CoreModule} from './core.module';
 import {SharedModule} from './shared/shared.module';
+import { StoreModule } from '@ngrx/store';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
+import {shoppingListReducer} from './shopping-list/store/shopping-list.reducer';
 
 
 @NgModule({
@@ -17,6 +19,7 @@ import {HeaderComponent} from './header/header.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     SharedModule,
     AppRoutingModule,
     CoreModule
